370776d77ca4fd08b91b472c3633d2a2b5e37858
congnguyendinh0 <44774137+congnguyendinh0@users.noreply.github.com>
2020-05-10 19:55:47 +0200

    Story 03 fixed cors  errors - cong,minh,manh
    fix cors

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const express =  require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors')
 app.use(bodyParser.json());
 const PORT = 5000;
 
 // Get routes
 const reportsRoute = require('./routes/reports');
 
+app.use(cors());
 // Use routes
 app.use('/api/reports', reportsRoute);
 
 // Set view engine
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
+    
 
     res.render('pages/index.ejs');
 });
